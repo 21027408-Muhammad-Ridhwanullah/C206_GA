b5917c367c164061fe9381a4cbb393d15592cee8
21027408 <21027408@LAPTOP-JUKBLISV>
2022-08-03 18:30:47 +0800

    Bike

--- C206_CaseStudy/src/BikersPortal.java
+++ C206_CaseStudy/src/BikersPortal.java
@@ -2,8 +2,42 @@
 public class BikersPortal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BikersPortal bp = new BikersPortal();
+		bp.start();
 
 	}
+	private void start() {
+		int option = 0;
+		
+	while(option != 6) {
+		menu();
+		if(option == 1 ) {}
+		else if(option == 2) {}
+		else if(option == 3) {}
+		else if(option == 4) {}
+		else if(option == 5) {}
+		else if(option == 6) {}
+		else {}
+	}
+	}
 
+	private void menu() {
+		Helper.line(80, "=");
+		System.out.println("BIKERS PORTAL");
+		Helper.line(80, "=");
+		
+		System.out.println("1. User");
+		System.out.println("2. Bike");
+		System.out.println("3. Bike Parts");
+		System.out.println("4. Appointment");
+		System.out.println("5. Feedback");
+		System.out.println("6. Quit");
+	}
+	
+	//Student 1
+	//student 2
+	//student 3
+	//student 4
+	//student 5
+	
 }
